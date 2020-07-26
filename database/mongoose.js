2d2d5f7bf4a0fed384637a5e843649ec4cdd7f20
shazaib491 <shazaibrehman127@gmail.com>
2020-07-27 01:28:56 +0530

    linked mongodb

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // ######connecting to database
-mongoose.connect('mongodb://127.0.0.1:27017/unique', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true
-}).then(() => {
-    console.log('Connected to database');
-}).catch((err) => {
-    console.log(err);
-})
-// ######connecting to database
-const secret="shazaib";
-module.exports=mongoose;
-module.exports.secret=secret;
+
+// mongoose.connect('mongodb://127.0.0.1:27017/unique', {
+mongoose.connect('mongodb+srv://shazaib:mau@6360@cluster0.2cuhh.mongodb.net/unique', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+    }).then(() => {
+        console.log('Connected to database');
+    }).catch((err) => {
+        console.log(err);
+    })
+    // ######connecting to database
+const secret = "shazaib";
+module.exports = mongoose;
+module.exports.secret = secret;
