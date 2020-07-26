da28750cbefb8edcd17fc298773b9bdb073935f9
shazaib491 <shazaibrehman127@gmail.com>
2020-07-27 01:31:27 +0530

    linked mongodb

--- database/mongoose.js
+++ database/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 // ######connecting to database
 
 // mongoose.connect('mongodb://127.0.0.1:27017/unique', {
-mongoose.connect('mongodb+srv://shazaib:mau@6360@cluster0.2cuhh.mongodb.net/unique', {
+mongoose.connect('mongodb+srv://shazaib:mausamash@cluster0.2cuhh.mongodb.net/unique', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: true
